2f795764dc5e161b408364f2f6acb398e3b7c15b
Aditya Raj Bisoyi <159037169+adityarajbisoyi@users.noreply.github.com>
2025-05-15 15:29:36 +0530

    added AI Integration

---
+++ herbarium/src/api/gemini.js
@@ -0,0 +1,33 @@
+// gemini.js
+import axios from 'axios';
+
+const API_KEY = import.meta.env.VITE_API_KEY;
+const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`;
+
+
+
+export async function getGeminiResponse(prompt) {
+    let limit = " give response under 100 words";
+    
+
+
+    console.log(prompt);
+    const data = {
+        contents: [{ parts: [{ text: prompt+limit }] }]
+
+    };
+
+    try {
+        const response = await axios.post(url, data, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+
+        const reply = response.data.candidates?.[0]?.content?.parts?.[0]?.text;
+        return reply || 'No response from Gemini';
+    } catch (error) {
+        console.error('Error:', error.response ? error.response.data : error.message);
+        return 'Error occurred while fetching response';
+    }
+}
