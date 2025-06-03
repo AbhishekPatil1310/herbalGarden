71c4dcc06566900265e21eb20ad1c795b0a9f183
AbhishekPatil1310 <ap7497493@gmail.com>
2025-06-03 19:52:21 +0530

    navbar css

--- herbarium/src/Components/navbar/Navbar.jsx
+++ herbarium/src/Components/navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 import NavItem from './NavItem';
 import AiIcon from './AiIcon';
+import '../../Style/Navbar.css';
 
 const Navbar = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/home';
 
   return (
-    <nav className="w-full flex items-center justify-between px-6 py-4 bg-white shadow-md sticky top-0 z-10">
-      <div className="flex items-center gap-6">
+    <nav className="navbar">
+      <div className="nav-items">
         <NavItem to="/home" label="Home" />
         <NavItem to="/world" label="Garden" />
         <NavItem to="/about" label="About" />
